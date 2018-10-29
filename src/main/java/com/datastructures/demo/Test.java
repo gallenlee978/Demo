a31975fe73f1e7a65885d1eed0300082712b5564
gallenlee978 <gallenlee978@163.com>
2018-10-29 15:12:54 +0800

    test

---
+++ src/main/java/com/datastructures/demo/Test.java
@@ -0,0 +1,13 @@
+package com.datastructures.demo;
+
+/**
+ * @author gallen
+ * @description test
+ * @date 2018/10/29
+ * @time 15:11
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
