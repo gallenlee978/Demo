6555b24ba468c2dfe61efe0d95967857585f10b8
gallenlee978 <gallenlee978@163.com>
2018-10-29 15:28:06 +0800

    testdemo

--- src/main/java/com/datastructures/demo/Test.java
+++ src/main/java/com/datastructures/demo/Test.java
@@ -9,5 +9,6 @@ package com.datastructures.demo;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Test Demo!");
     }
 }
